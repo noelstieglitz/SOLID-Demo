18afbc56acc079d3673b7dda4c17767dbc6fdfa9
Noel Stieglitz <noel@componotech.com>
2013-12-17 12:15:02 -0500

    SRP initial push

---
+++ After/InsuranceApplicant.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace After
+{
+    public class InsuranceApplicant
+    {
+        public InsuranceApplicant(DateTime bornOnDate)
+        {
+            BornOnDate = bornOnDate;
+            Id = Guid.NewGuid();
+        }
+
+        public DateTime BornOnDate { get; private set; }
+        public Guid Id { get; set; }
+
+        public decimal CalculatePremium()
+        {
+            if (BornOnDate.Year <= 1950)
+            {
+                return 1000m;
+            }
+
+            return 500m;
+        }
+    }
+
+    public class InsuranceApplicantRepository
+    {
+        public HashSet<InsuranceApplicant> Applicants { get; set; }
+        public void Save(InsuranceApplicant applicant)
+        {
+            Applicants.Add(applicant);
+        }
+    }
+}
---
+++ Before/InsuranceApplicant.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Before
+{
+    public class InsuranceApplicant
+    {
+        public HashSet<InsuranceApplicant> Applicants { get; set; }
+
+        public InsuranceApplicant(DateTime bornOnDate)
+        {
+            BornOnDate = bornOnDate;
+            Id = Guid.NewGuid();
+        }
+
+        public DateTime BornOnDate { get; private set; }
+        public Guid Id { get; set; }
+
+        public decimal CalculatePremium()
+        {
+            if (BornOnDate.Year <= 1950)
+            {
+                return 1000m;
+            }
+
+            return 500m;
+        }
+
+        public void SaveToRepository()
+        {
+            Applicants.Add(this);
+        }
+    }
+}
