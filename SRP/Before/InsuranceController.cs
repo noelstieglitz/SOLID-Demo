6691b7d59109611bb39c39c0b0cb65b3e561a0d2
Noel Stieglitz <noel@componotech.com>
2014-01-02 12:40:12 -0500

    minor update

---
+++ SRP/Before/InsuranceController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Before
+{
+    public class InsuranceController
+    {
+        public void Run()
+        {
+            Console.WriteLine("Starting up...");
+            var applicant = new InsuranceApplicant(new DateTime(1973, 1, 13), 32000);
+
+            Console.WriteLine("Calculating premium...");
+            applicant.CalculatePremium();
+            Console.WriteLine("Premium is {0}", applicant.Premium);
+
+            Console.WriteLine("Saving to store...");
+            applicant.SaveToStore();
+            Console.WriteLine("Done.");
+
+            Console.Read();
+        }
+    }
+}
--- SRP/Before/Program.cs
+++ SRP/Before/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,18 +9,8 @@ namespace Before
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Starting up...");
-            var applicant = new InsuranceApplicant(new DateTime(1973, 1, 13), 32000);
-
-            Console.WriteLine("Calculating premium...");
-            applicant.CalculatePremium();
-            Console.WriteLine("Premium is {0}", applicant.Premium);
-
-            Console.WriteLine("Saving to store...");
-            applicant.SaveToStore();
-            Console.WriteLine("Done.");
-
-            Console.Read();
+            var controller = new InsuranceController();
+            controller.Run();
         }
     }
 }
